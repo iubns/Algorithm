1f092c484034d17aab9239ddf994790812c01ca5
iubns <iubns@naver.com>
2026-02-02 18:14:54 +0900

    bj: 1138

---
+++ BaekJoonCpp/1138/Main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    return 0;
+}
