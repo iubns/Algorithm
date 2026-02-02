8cca93d375dc2a7ede3208a31791111bd618e29a
iubns <iubns@naver.com>
2026-02-02 23:48:55 +0900

    bj: 15649

---
+++ BaekJoonCpp/15649/Main.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+int selected[9] = {0,};
+
+void next(int step, int n, int length){
+    if(step == 0){
+        for(int i = 0; i < length; i++){
+            printf("%d ", selected[i]);
+        }
+        printf("\n");
+        return;
+    }
+
+    for(int i = 1; i <= n; i++){
+        bool out = false;
+        for(int j = 0; j < length; j++){
+            if(selected[j] == i){
+                out = true;
+                break;
+            }
+        }
+        if(out){
+            continue;
+        }
+
+        selected[length - step] = i;
+        next(step - 1, n, length);
+        selected[length - step] = 0;
+    }
+}
+
+int main(){
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(NULL);
+
+
+    int max, length;
+    cin >> max >> length;
+    next(length, max, length);
+}
